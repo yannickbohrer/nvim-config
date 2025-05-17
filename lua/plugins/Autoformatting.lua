c0f3af32ecb612a8c419623cb8653b086f1ac662
yannickbohrer <bohreryannick@gmail.com>
2025-05-17 17:33:16 +0200

    resolves mason automatic_install issue

--- lua/plugins/Autoformatting.lua
+++ lua/plugins/Autoformatting.lua
@@ -1,28 +1,25 @@
--- Format on save and linters
 return {
 	"nvimtools/none-ls.nvim",
 	dependencies = {
 		"nvimtools/none-ls-extras.nvim",
-		"jayp0521/mason-null-ls.nvim", -- ensure dependencies are installed
+		"jayp0521/mason-null-ls.nvim",
 	},
 	config = function()
 		local null_ls = require("null-ls")
-		local formatting = null_ls.builtins.formatting -- to setup formatters
-		local diagnostics = null_ls.builtins.diagnostics -- to setup linters
+		local formatting = null_ls.builtins.formatting
+		local diagnostics = null_ls.builtins.diagnostics
 
-		-- list of formatters & linters for mason to install
+		-- Manual null-ls tool list (removed mason-null-ls automatic_installation)
 		require("mason-null-ls").setup({
 			ensure_installed = {
 				"checkmake",
-				"prettier", -- ts/js formatter
-				"stylua", -- lua formatter
-				"eslint_d", -- ts/js linter
+				"prettier",
+				"stylua",
+				"eslint_d",
 				"shfmt",
 				"ruff",
-				"clang-format", -- c++ formatter
+				"clang-format",
 			},
-			-- auto-install configured formatters & linters (with null-ls)
-			automatic_installation = true,
 		})
 
 		local sources = {
@@ -51,15 +48,13 @@ return {
 			require("none-ls.formatting.ruff").with({ extra_args = { "--extend-select", "I" } }),
 			require("none-ls.formatting.ruff_format"),
 			formatting.clang_format.with({
-				filetypes = { "cpp", "c", "objc", "objcpp" }, -- Ensure it applies to C++ files
+				filetypes = { "cpp", "c", "objc", "objcpp" },
 			}),
 		}
 
 		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 		null_ls.setup({
-			-- debug = true, -- Enable debug mode. Inspect logs with :NullLsLog.
 			sources = sources,
-			-- you can reuse a shared lspconfig on_attach callback here
 			on_attach = function(client, bufnr)
 				if client.supports_method("textDocument/formatting") then
 					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
